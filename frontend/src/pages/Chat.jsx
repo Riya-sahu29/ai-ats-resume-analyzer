1d57c5d6ffe40d3f55b1b9500871f5dbc240a87e
Riya-sahu29 <justriya2004@gmail.com>
2026-03-20 20:06:19 +0530

    Added logoai.svg and Chat.jsx

---
+++ frontend/src/pages/Chat.jsx
@@ -0,0 +1,9 @@
+import Chatbot from "../components/Chatbot";
+
+export default function Chat() {
+  return (
+    <div className="min-h-screen bg-blue-50 flex items-center justify-center p-10">
+      <Chatbot />
+    </div>
+  );
+}
