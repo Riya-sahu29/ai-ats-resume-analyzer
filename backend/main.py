fca322cd1dcf6c9ca87db84e013e90556c859fd3
Riya priyadarsnai sahu <justriya2004@gmail.com>
2026-04-04 16:38:03 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 import PyPDF2
 
 from backend.resume_ai import analyze_resume_with_ai
-from chatbot import chat_with_ai
+from backend.chatbot import chat_with_ai
 from pydantic import BaseModel
 
 app = FastAPI()
