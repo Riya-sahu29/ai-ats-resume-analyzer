535122c12b654355b1670702a26a1a711dd71030
Riya priyadarsnai sahu <justriya2004@gmail.com>
2026-04-24 12:14:02 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -3,17 +3,9 @@ import os
 
 MONGO_URL = os.getenv("MONGO_URL")
 
-if not MONGO_URL:
-    raise Exception("MONGO_URL is not set")
-
-client = MongoClient(MONGO_URL, serverSelectionTimeoutMS=5000)
-
-# Force connection
-client.server_info()
+client = MongoClient(MONGO_URL)
 
 db = client["ai_resume_analyzer"]
 
 chat_collection = db["chat_history"]
 resume_collection = db["resume_data"]
-
-print("MongoDB Connected Successfully")
