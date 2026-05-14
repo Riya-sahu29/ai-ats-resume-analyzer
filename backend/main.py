97501eb3fffad8633cae5cf7c7adc31db4495d07
Riya priyadarsnai sahu <justriya2004@gmail.com>
2026-05-14 13:00:39 +0530

    fix the comma

--- backend/main.py
+++ backend/main.py
@@ -36,7 +36,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "https://ai-ats-resume-analyzer-xi.vercel.app",
-    ]
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
